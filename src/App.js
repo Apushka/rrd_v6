a0355d489c29c7ef9c131980740af7d80283baaa
apushkaa@gmail.com <aleksandrpushkin@MacBook-Air-Aleksandr.local>
2022-11-06 21:53:52 +0300

    rrd_v6 initial

---
+++ src/App.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { Link, Navigate, Outlet, useParams, useRoutes } from "react-router-dom";
+
+const UsersLayout = () => {
+  return (
+    <div>
+      <h1>Users Layout</h1>
+      <Link to="/">Main Page</Link>
+      <Outlet />
+    </div>
+  );
+};
+
+const UserListPage = () => {
+  const [users] = useState([1, 2, 3, 4, 5]);
+  return (
+    <>
+      <h1>Users List Page</h1>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>
+            <Link to={`/users/${user}`}>User {user}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const UserInfoPage = () => {
+  const { userId } = useParams();
+  return (
+    <>
+      <h1>User Page</h1>
+      <ul>
+        <li>
+          <Link to="/users">Users List Page</Link>
+        </li>
+        <li>
+          <Link to={`/users/${userId}/edit`}>Edit this user</Link>
+        </li>
+      </ul>
+      <p>userId: {userId}</p>
+    </>
+  );
+};
+
+const EditUserPage = () => {
+  const { userId } = useParams();
+  return (
+    <>
+      <h1>Edit User Page</h1>
+      <ul>
+        <li>
+          <Link to={`/users/${userId}/profile`}>User Profile Page</Link>
+        </li>
+        <li>
+          <Link to={`/users/${Number(userId) + 1}/profile`}>Another User</Link>
+        </li>
+        <li>
+          <Link to={`/users`}>Users List Page</Link>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+const MainPage = () => {
+  return <h1>Main Page</h1>;
+};
+
+const CustomNavigate = () => {
+  const { userId } = useParams();
+  return <Navigate to={`/users/${userId}/profile`} />;
+};
+
+const routes = [
+  { path: "/", element: <MainPage /> },
+  {
+    path: "users",
+    element: <UsersLayout />,
+    children: [
+      { path: "", element: <UserListPage /> },
+      {
+        path: ":userId",
+        children: [
+          { path: "", element: <CustomNavigate /> },
+          { path: "profile", element: <UserInfoPage /> },
+          { path: "edit", element: <EditUserPage /> },
+          { path: "*", element: <CustomNavigate /> },
+        ],
+      },
+    ],
+  },
+  { path: "*", element: <Navigate to="/" /> },
+];
+
+function App() {
+  const elements = useRoutes(routes);
+  return (
+    <>
+      <h1>App Layout</h1>
+      <Link to="/users">Users List Page</Link>
+      {elements}
+    </>
+  );
+}
+
+export default App;
